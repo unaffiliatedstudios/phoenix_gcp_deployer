013b73cc2036ce87bc3e88cf8e9c1a4c430bb4f7
Cedric Johnson <cjohns0913@gmail.com>
2026-02-16 12:32:50 -0700

    Adds Tidewave

--- lib/phoenix_gcp_deployer_web/endpoint.ex
+++ lib/phoenix_gcp_deployer_web/endpoint.ex
@@ -11,6 +11,10 @@ defmodule PhoenixGcpDeployerWeb.Endpoint do
     same_site: "Lax"
   ]
 
+  if Code.ensure_loaded?(Tidewave) do
+    plug Tidewave
+  end
+
   socket "/live", Phoenix.LiveView.Socket,
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
